3c4a7b28ef34730a78f8b9f3df738ec83c75eeda
Arun-Selv-02136 <arunakilselva@gmail.com>
2025-11-21 13:35:56 +0530

    homepage and hassrouter add

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,18 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Navigate } from 'react-router-dom'
-import { BrowserRouter , Route,Routes } from 'react-router-dom'
+import { HashRouter , Route,Routes } from 'react-router-dom'
 import Register from './pages/Register.jsx'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
       <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/Register" element={<Register/>}/>
           <Route path="/Login" element={<Login />}/>
           <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
